0dc85e2cadb4a670f0fa882f0e6cfe4d27fce40b
Tagoresatya <tagoresatya2022@gmail.com>
2026-07-25 09:18:29 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -10,23 +10,6 @@ import os
 
 
 
-# --------------------
-# App Initialization
-# --------------------
-app = Flask(__name__)
-app.secret_key = "myverysecretkey"  # change this in production
-
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-
-app.config['MAIL_DEFAULT_SENDER'] = 'tagoresatya2022@gmail.com'
-
-mail = Mail(app)
-serializer = URLSafeTimedSerializer(app.secret_key)
 
 # --------------------
 # Database Connection Helper
